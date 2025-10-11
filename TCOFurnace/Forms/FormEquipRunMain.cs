b42267b677948670889fe8eb82d4960d9e233a12
xzb <2122678029@qq.com>
2025-10-11 22:08:05 +0800

    1.标准模式,鱼类，肉类等模式 提供后门修改对应的反应参数(隐藏页面需要输入密  码，密码为电脑时间 年月日 数字相加乘以5)。

--- TCOFurnace/Forms/FormEquipRunMain.cs
+++ TCOFurnace/Forms/FormEquipRunMain.cs
@@ -22,6 +22,19 @@ namespace TCOFurnace.Forms
 {
     public partial class FormEquipRunMain : BaseForm
     {
+        #region 在左上角连续点击7次弹框输入密码
+        // 点击计数器
+        private int clickCount = 0;
+        private int clickCount1 = 0;
+        // 记录上次点击时间
+        private DateTime lastClickTime = DateTime.MinValue;
+        // 定义有效点击时间窗口（毫秒）
+        private int ClickTimeWindow = 1500; // 1.5秒内
+        // 定义需要点击的区域（这里设置为一个200x200的区域，位于窗体左上角）
+        private readonly System.Drawing.Rectangle targetArea;
+        #endregion
+
+
         //两路仪器
         public static readonly List<StateInstrument> stateInstruments = new List<StateInstrument>();
         //当前页面的数据信息
@@ -40,6 +53,59 @@ namespace TCOFurnace.Forms
 
             //仪器属性有变化通知页面
             stateInstruments.ForEach(instr => AddPropertyChanged(instr.monitoringData));
+
+            // 初始化目标区域（左上角200x200的区域）
+            targetArea = new System.Drawing.Rectangle(10, 10, 200, 200);
+            // 为窗体添加点击事件
+            this.MouseDown += SevenClickForm_MouseDown;
+        }
+
+        /// <summary>
+        /// 在左上角连续点击7次弹框输入密码
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SevenClickForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            clickCount1++;
+            // 检查点击是否在目标区域内
+            if (targetArea.Contains(e.Location))
+            {
+                DateTime currentTime = DateTime.Now;
+
+                // 检查是否在有效时间窗口内
+                if (currentTime.Subtract(lastClickTime).TotalMilliseconds < ClickTimeWindow)
+                {
+                    // 在时间窗口内，增加计数
+                    clickCount++;
+                }
+                else
+                {
+                    // 超出时间窗口，重置计数
+                    clickCount = 1;
+                }
+
+                // 更新上次点击时间
+                lastClickTime = currentTime;
+
+                // 检查是否达到七次点击
+                if (clickCount >= 7)
+                {
+                    var passwordForm = new PasswordForm();
+                    DialogResult result = passwordForm.ShowDialog();
+                    if (result == DialogResult.OK)
+                    {
+                        butParaSet.Visible = true;
+                    }
+                    // 重置计数器
+                    clickCount = 0;
+                }
+            }
+            else
+            {
+                // 点击不在目标区域，重置计数
+                clickCount = 0;
+            }
         }
 
         /// <summary>
---
+++ TCOFurnace/Forms/PasswordForm.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using TCOFurnace.DataService;
+using TCOFurnace.Models;
+using TCOFurnace.Common;
+using static System.Collections.Specialized.BitVector32;
+using Common;
+using System.Data.SqlTypes;
+
+namespace TCOFurnace.Forms
+{
+    public partial class PasswordForm : Form
+    {
+        private System.ComponentModel.IContainer components = null;
+        private Label lblPassword;
+        private TextBox txtPassword;
+        private Button ButAdd;
+        private Button BtnCancel;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        public PasswordForm()
+        {
+            InitializeComponent();
+
+      
+        }
+       
+        private void InitializeComponent()
+        {
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.ButAdd = new System.Windows.Forms.Button();
+            this.BtnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(12, 42);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(65, 12);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "密    码：";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(83, 39);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(140, 21);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // ButAdd
+            // 
+            this.ButAdd.Location = new System.Drawing.Point(73, 85);
+            this.ButAdd.Name = "ButAdd";
+            this.ButAdd.Size = new System.Drawing.Size(75, 21);
+            this.ButAdd.TabIndex = 4;
+            this.ButAdd.Text = "确定";
+            this.ButAdd.UseVisualStyleBackColor = true;
+            this.ButAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.BtnCancel.Location = new System.Drawing.Point(154, 85);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(69, 21);
+            this.BtnCancel.TabIndex = 5;
+            this.BtnCancel.Text = "取消";
+            this.BtnCancel.UseVisualStyleBackColor = true;
+            this.BtnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // PasswordForm
+            // 
+            this.AcceptButton = this.ButAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.BtnCancel;
+            this.ClientSize = new System.Drawing.Size(235, 134);
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.ButAdd);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "PasswordForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "密码验证";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            //密码为年月日数字之和乘以5
+
+            // 获取当前日期
+            DateTime today = DateTime.Today;
+
+            // 提取年、月、日
+            int year = today.Year;
+            int month = today.Month;
+            int day = today.Day;
+
+            // 计算各位数字之和
+            int sum = year + month + day;
+            if (int.TryParse(txtPassword.Text, out int result) && result==(year + month + day)*5)
+            {
+                this.DialogResult = DialogResult.OK;
+            }
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+    }
+}
